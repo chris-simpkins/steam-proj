ae760efd1c19bb72750dae4bfa63fdd12beef9fe
unknown <chris.simpkins@squaredup.com>
2018-10-10 23:05:14 +0100

    accepts incoming trades, started random trades

--- bot.js
+++ bot.js
@@ -1,19 +1,76 @@
 const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
+const steamCommunity = require('steamcommunity');
+const tradeOfferManager = require('steam-tradeoffer-manager');
 
 const client = new steamUser();
+const community = new steamCommunity();
+const manager = new tradeOfferManager({
+    steam: client,
+    community: community,
+    language: 'en'
+});
 
 const logOnOptions = {
-    accountName: process.env.USERNAME,
-    password: process.env.PASSWORD,
+    accountName: 'username',
+    password: 'password',
 
-    //twoFactor: steamTotp.generateAuthCode('https://www.reddit.com/r/SteamBot/comments/3w5zwb/info_get_your_2fa_codes_from_android_no_root/')
+    twoFactorCode: steamTotp.generateAuthCode('shared_secret')
 };
+
+function sendRandomItem() {
+    manager.loadInventory(440, 2, true, (err, inventory) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const offer = manager.createOffer('trustedfriendid');
+            const item = inventory[Math.floor(Math.random() * inventory.length - 1)];
+
+            offer.addMyItem(item);
+            offer.setMessage(`item: ${item.name}`);
+            offer.send((err, status) => {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(`Sent offer. status ${status}`);
+                }
+            });
+        }
+    });
+}
 
 client.logOn(logOnOptions);
 
 client.on('loggedOn', () => {
     console.log('success');
 
-    client.setPersona(steamUser.Steam.EPersonaState.Online, 'big mens');
+    client.setPersona(steamUser.Steam.EPersonaState.Online, 'christestbot');
+});
+
+client.on('webSession', (sessionid, cookies) => {
+    manager.setCookies(cookies);
+
+    community.setCookies(cookies);
+    community.startConfirmationChecker(10000, 'identity_secret');
+    //sendRandomItem();
+});
+
+manager.on('newOffer', offer => {
+    if (offer.partner.getSteamID64() === 'trustedfriendid') {
+        offer.accept((err, status) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(`Accepted offer, status: ${status}`);
+            }
+        });
+    } else {
+        offer.decline(err => {
+            if(err) {
+                console.log(err);
+            } else {
+                console.log('Canceled offer');
+            }
+        });
+    }
 });
