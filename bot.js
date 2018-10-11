04437250e133ae6b899b53fdeae6f335b959d993
Chris Simpkins <Chris.Simpkins@squaredup.com>
2018-10-11 13:51:04 +0100

    got .env file working

--- bot.js
+++ bot.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
 const steamCommunity = require('steamcommunity');
@@ -12,11 +13,13 @@ const manager = new tradeOfferManager({
 });
 
 const logOnOptions = {
-    accountName: 'username',
-    password: 'password',
+    accountName: process.env.PRIMARY_USER,
+    password: process.env.PRIMARY_PASSWORD,
 
-    twoFactorCode: steamTotp.generateAuthCode('shared_secret')
+    twoFactorCode: steamTotp.generateAuthCode(process.env.PRIMARY_SECRET)
 };
+
+console.log(process.env.PRIMARY_USER);
 
 function sendRandomItem() {
     manager.loadInventory(440, 2, true, (err, inventory) => {
@@ -44,7 +47,7 @@ client.logOn(logOnOptions);
 client.on('loggedOn', () => {
     console.log('success');
 
-    client.setPersona(steamUser.Steam.EPersonaState.Online, 'christestbot');
+    client.setPersona(steamUser.Steam.EPersonaState.Online);
 });
 
 client.on('webSession', (sessionid, cookies) => {
